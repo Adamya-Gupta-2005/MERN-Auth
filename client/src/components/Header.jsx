b9e50faf5438906e3cc518b1d84d7c45ce64097b
Adamya Gupta <adamyagupta005@gmail.com>
2026-06-29 22:11:34 +0530

    Finalized styles for css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { AppContext } from '../context/AppContext';
 function Header() {
   const {userData} = useContext(AppContext);
   return (
-    <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
+    <div className='flex flex-col items-center mt-36 px-4 text-center text-gray-800'>
       <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey {userData?userData.name:'Developer'}
         <img src={assets.hand_wave} className='w-8 aspect-square' alt="" /></h1>
       <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our app</h2>
